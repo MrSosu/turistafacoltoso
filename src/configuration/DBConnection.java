ce7add8651f775bab3756c68b74b30c80f0b1128
m_adriani <m.adriani@jdk.it>
2025-01-09 13:44:27 +0100

    committino

---
+++ src/configuration/DBConnection.java
@@ -0,0 +1,9 @@
+package configuration;
+
+public class DBConnection {
+
+    private final String USERNAME = "root";
+    private final String PASSWORD = "Trucebaldanzi35";
+    private final String URL = "jdbc:mysql://localhost:3306/turistafacoltoso";
+
+}
